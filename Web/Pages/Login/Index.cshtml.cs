8d8be77e507a625adb6d4f1c3fa9e7cec6b1075d
20241TIIMI0372 <davicafr@gmail.com>
2025-10-16 10:50:55 -0300

    tela de login

---
+++ Web/Pages/Login/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web.Pages.NovaPasta
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
