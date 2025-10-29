440efb7cd7b0df552556ec4f60b872b49e4cebde
Filippi Kruger Rigo <20231TIIMI0022@cefetes.br>
2025-10-29 12:22:27 -0300

    bruhmit

--- Infra/Database/TabUsuarios.cs
+++ Infra/Database/TabUsuarios.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Infra.DataBase;
 
@@ -11,5 +12,6 @@ public partial class TabUsuarios
 
     public string Cargo { get; set; } = null!;
 
+    [DataType(DataType.Password)]
     public string Senha { get; set; } = null!;
 }
--- Web/Pages/Login/Index.cshtml.cs
+++ Web/Pages/Login/Index.cshtml.cs
@@ -1,20 +1,39 @@
-using Infra.DataBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Infra.DataBase;
+using System.Net.Mail;
 namespace Web.Pages.NovaPasta
 {
     public class IndexModel : PageModel
     {
-        [BindProperty]
-        public TabUsuarios Usuario { get; set; }
         public void OnGet()
         {
         }
 
+        private readonly Infra.DataBase.STC_Context _context;
+
+        public IndexModel(Infra.DataBase.STC_Context context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public TabUsuarios Usuario { get; set; }
+
+
         public void OnPost()
         {
+            if (!ModelState.IsValid)
+                return;
 
+            var usuarios = _context.TabUsuarios.ToList();
+
+            
         }
     }
 }
