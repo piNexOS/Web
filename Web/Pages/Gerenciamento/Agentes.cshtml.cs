812b6a5c655cab2d18fee19408e5fa846d4cf784
Andre <andresiller.es@gmail.com>
2025-12-02 00:56:03 -0300

    -> Criação do endereço Gerenciamento/Agentes
    -> Alteração do link Gerenciamento/Funcionarios para gerenciamento/agentes (além de alterar o nome do link, de funcionários para agentes de campo)
    -> Qtd de OS programadas no gerenciamento de agentes

---
+++ Web/Pages/Gerenciamento/Agentes.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+using Microsoft.AspNetCore.Authorization;
+
+
+namespace Web.Pages.Gerenciamento
+{
+    [Authorize(Roles = "Gerente,Administrador,Programador")]
+    public class Agentes : PageModel
+    {
+        private readonly Infra.DataBase.STC_Context _context;
+
+        public Agentes(Infra.DataBase.STC_Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Infra.DataBase.TabAgentes> TabAgentes { get; set; } = default!;
+        public IList<Infra.DataBase.RoteiroDetalhes> TabRoteiroDetalhes { get; set; } = default!;
+        public IList<Infra.DataBase.Roteiros> TabRoteiros { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            TabAgentes = await _context.TabAgentes
+                .Include(a => a.Roteiros)
+                    .ThenInclude(r => r.RoteiroDetalhes)
+                .ToListAsync();
+        }
+    }
+}
+
