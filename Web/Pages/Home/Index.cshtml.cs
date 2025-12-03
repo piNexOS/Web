84f03ecbe4b65fa5a822dca5c2cd7ea2fd53b13e
Andre <andresiller.es@gmail.com>
2025-12-03 18:29:48 -0300

    Tabela de agentes ativos na home

--- Web/Pages/Home/Index.cshtml.cs
+++ Web/Pages/Home/Index.cshtml.cs
@@ -1,14 +1,32 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
 
 namespace Web.Pages.Home
 {
     [Authorize]
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly Infra.DataBase.STC_Context _context;
+
+        public IndexModel(Infra.DataBase.STC_Context context)
         {
+            _context = context;
+        }
+
+        public IList<Infra.DataBase.TabAgentes> TabAgentes { get; set; } = default!;
+        public IList<Infra.DataBase.RoteiroDetalhes> TabRoteiroDetalhes { get; set; } = default!;
+        public IList<Infra.DataBase.Roteiros> TabRoteiros { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            TabAgentes = await _context.TabAgentes
+                .Where(a => a.Status == "Ativo")
+                .Include(a => a.Roteiros)
+                    .ThenInclude(r => r.RoteiroDetalhes)
+                        .ThenInclude(d => d.IdOrdemServicoNavigation)
+                .ToListAsync();
         }
     }
 }
